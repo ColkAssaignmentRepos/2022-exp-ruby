a65317384aaf7ae74c5c81c750b42990af79c4e6
Colk-tech <colk.tech@gmail.com>
2022-09-28 17:11:36 +0900

    add: Complete the `18` assiggnment

---
+++ code/18.rb
@@ -0,0 +1,22 @@
+def rev(array)
+  # 型バリデーション
+  unless array.is_a?(Array)
+    raise TypeError, "expected 'Array' type variable, but got #{array.class}"
+  end
+
+  reversed = []
+
+  length = array.length
+
+  (length-1).downto(0).each do |i|
+    reversed.push(array[i])
+  end
+
+  array = reversed
+
+  return reversed
+end
+
+if __FILE__ == $0
+  print rev([0,1,2,3,4,5])
+end
